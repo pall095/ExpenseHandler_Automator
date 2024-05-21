f11c3e79ab3f02c643616efb7d931b551e8c12d2
LAPTOP-H4U519FK\matte <matteopallomo@gmail.com>
2024-05-21 21:09:51 +0200

    Corrected path

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,7 +28,7 @@ def findMatch_onClick( input_descr , expense_database  ) :
         print( "No match!")
 
 # Data management
-categories_file = r"C:\Users\matte\Desktop\Git\Pall0sProjects\gSheetAutomation-Streamlit\UsciteCTRL_19Mag2024.csv"
+categories_file = r"C:\Users\matte\Desktop\Git\ExpenseHandler_Automator\UsciteCTRL_19Mag2024.csv"
 cat_database = utl.createCatDatabase( categories_file )
 
 # Web App Management
